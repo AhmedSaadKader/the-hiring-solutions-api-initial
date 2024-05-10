883f4541a0110ce9ebaa8c3c7e6d8004b7f94a6a
Ahmed Saad Abd El-Kader <ahmed.saad.kader@gmail.com>
2024-05-11 01:28:25 +0300

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+
+const app: express.Application = express();
+const address: string = process.env.PORT || '0.0.0.0:3000';
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', function (req: Request, res: Response) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log(`starting app on: ${address}`);
+});
